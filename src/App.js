c031c992c2fc726353cbb8099d08d2421fe45bf1
Ega Radiegtya <radiegtya@Egas-MacBook-Pro.local>
2017-09-29 06:51:47 +0700

    7.eventws

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
 import React, {Component} from 'react';
-import Coffee from './components/Coffee';
-import Glass from './components/Glass';
-import Saucer from './components/Saucer';
 
 export default class App extends Component{
 
-  render(){
-    console.log('render');
-    return (
-      <div>
-        <Coffee
-          waterVolume={1}
-          bean="robusta"
-          type="expresso"
-        />
-        <Glass color="red"/>
-        <Saucer width={100}/>
-      </div>
-    )
+  handleClick(){
+    alert('I am clicked!');
+    console.log('I am clicked!');
   }
 
+  render(){
+    return (
+      <button
+        onClick={()=> this.handleClick()}
+      >
+        Click Me!
+      </button>
+    )
+  }
 }
