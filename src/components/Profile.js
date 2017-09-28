91aa916eca008f07e56d9f933625ba425be20e4a
Ega Radiegtya <radiegtya@Egas-MacBook-Pro.local>
2017-09-29 06:13:21 +0700

    4. Child Component

---
+++ src/components/Profile.js
@@ -0,0 +1,17 @@
+import React, {Component} from 'react';
+
+export default class Profile extends Component{
+
+  render(){
+      return (
+        <div>
+          <ul>
+            <li>Nama: Gina</li>
+            <li>Umur: 19</li>
+            <li>Jenis Kelamin: Perempuan</li>
+          </ul>
+        </div>
+      );
+  }
+
+}
